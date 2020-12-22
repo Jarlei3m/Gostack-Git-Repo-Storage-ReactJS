cdd4f85d3344c1a336511c6465857e0e8f805729
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2020-12-22 14:40:58 -0300

    repos&issues list

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FaSpinner } from 'react-icons/fa'
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import Container from '../components/Container';
+import { Loading, Owner, IssueList } from './styles';
 
 class Repository extends Component {
   static propTypes = {
@@ -38,7 +41,7 @@ class Repository extends Component {
     ]);
 
     this.setState({
-      respository: repository.data,
+      repository: repository.data,
       issues: issues.data,
       loading: false,
     });
@@ -47,7 +50,40 @@ class Repository extends Component {
   render() {
     const { repository, issues, loading } = this.state;
 
-    return <h1>Repository</h1>;
+    if (loading) {
+      return (
+      <Loading>
+        <FaSpinner />
+      </Loading>
+    )}
+
+    return (
+      <Container>
+        <Owner>
+        <Link to="/">Return to repositories</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login}/>
+              <div>
+                <strong>
+                  <a target="_blank" href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
 
